68a21d6c7c27080e652ae8c458fd8143be0053a1
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-10-21 13:08:24 +0100

    feat: added model to regiter StringAnalysis in the model.py

--- strings/models.py
+++ strings/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import json 
 
 # Create your models here.
+
+class StringAnalysis(models.Model):
+    id = models.CharField(max_length=64, primary_key=True)  # SHA-256 hash
+    value = models.TextField()
+    length = models.IntegerField()
+    is_palindrome = models.BooleanField()
+    unique_characters = models.IntegerField()
+    word_count = models.IntegerField()
+    character_frequency_map = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'string_analysis'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.value[:50]}... ({self.id})"
